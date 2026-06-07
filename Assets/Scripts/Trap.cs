d9ef0af72023eab86cab5fab42d189bc7542e90c
TWITexe <kawaii_senpaii@mail.ru>
2026-06-07 08:15:40 +0700

    chore(debugs)

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -13,13 +13,11 @@ public class Trap : NetworkBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"ENTER {other.name}");
         if (!isServer) return; // обработка только на сервере
 
         Health health = other.GetComponentInParent<Health>();
         if (health != null)
         {
-            Debug.Log($"FOUND HEALTH {health.name}");
             Coroutine coroutine = StartCoroutine(DamageOverTime(health));
             damageCoroutines.Add(health, coroutine);
         }
@@ -41,7 +39,6 @@ public class Trap : NetworkBehaviour
     {
         while (true)
         {
-            Debug.Log($"DAMAGE TICK {health.name}");
             if (health == null)
                 yield break;
 
